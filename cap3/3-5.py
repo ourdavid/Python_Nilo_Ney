26030fd8565a508f0b4c9cc150b15a0ca35c62dc
David William <sysd4v1d@gmail.com>
2025-09-11 10:10:57 -0300

    atividade 3-5

---
+++ cap3/3-5.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+c = True
+d = False
+
+print( "Resultado: ", a > b and c or d) #false and true = false
+
+a = 10
+b = 3
+c = False
+d = False
+
+print( "Resultado: ", a > b and c or d) # false
+
+a = 5
+b = 1
+c = True
+d = True
+print( "Resultado: ", a > b and c or d)
